2ff1b28dff959fdc6892ed6409f7ddaad27b0a04
Keila Oliveira <keila.oliver08@gmail.com>
2019-03-31 18:51:46 -0300

    Inserindo a página com os detalhes dos produtos

---
+++ src/pages/produto-detalhe/produto-detalhe.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProdutoDTO } from '../../models/produto.dto';
+
+@IonicPage()
+@Component({
+  selector: 'page-produto-detalhe',
+  templateUrl: 'produto-detalhe.html',
+})
+export class ProdutoDetalhePage {
+
+  item: ProdutoDTO;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.item = {
+      idProduto: "1",
+      descricaoProduto: "Mouse",
+      preco: 80.59
+    }
+  }
+
+}
