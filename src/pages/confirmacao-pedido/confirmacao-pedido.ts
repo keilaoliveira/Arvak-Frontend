d397d65669e41e15ab887f6fc14ec9a189b9dfca
Keila Oliveira <keila.oliver08@gmail.com>
2019-04-07 19:40:48 -0300

    Salvar pedido

--- src/pages/confirmacao-pedido/confirmacao-pedido.ts
+++ src/pages/confirmacao-pedido/confirmacao-pedido.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -23,7 +24,8 @@ export class ConfirmacaoPedidoPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
 
     this.pedido = this.navParams.get('pedido');
 
@@ -51,4 +53,20 @@ export class ConfirmacaoPedidoPage {
     return this.cartService.total();
   }
 
+  back(){
+   this.navCtrl.setRoot('CartPage'); 
+  }
+
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location'));
+      }, 
+      error => {
+        if (error.status == 403){
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+  }
 }
--- src/pages/escolher-endereco/escolher-endereco.module.ts
+++ src/pages/escolher-endereco/escolher-endereco.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { EscolherEnderecoPage } from './escolher-endereco';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @NgModule({
   declarations: [
