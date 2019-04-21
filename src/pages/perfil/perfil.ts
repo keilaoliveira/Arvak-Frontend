c9f003479465ff368c19608fdeb8245fc23b5b00
Keila Oliveira <keila.oliver08@gmail.com>
2019-04-21 17:36:46 -0300

    Corrigindo reload da imagem de perfil

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -5,6 +5,7 @@ import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CameraOptions, Camera } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -15,6 +16,7 @@ export class PerfilPage {
 
   cliente: ClienteDTO;
   picture: string;
+  profileImage;
   cameraOn: boolean = false;
 
   constructor(
@@ -23,8 +25,11 @@ export class PerfilPage {
     public storage: StorageService,
     public clienteService: ClienteService,
     public camera: Camera,
-    public loadingCtrl: LoadingController
-  ) {
+    public loadingCtrl: LoadingController,
+    public sanitizer: DomSanitizer) {
+
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+
   }
 
   ionViewDidLoad() {
@@ -55,9 +60,27 @@ export class PerfilPage {
     this.clienteService.getImageFromBucket(this.cliente.idCliente)
       .subscribe(response => {
         this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.idCliente}.jpg`;
+        this.blobToDataURL(response).then(dataUrl => {
+          let str: string = dataUrl as string;
+          this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+        });
       },
-        error => { });
+        error => {
+          this.profileImage = 'assets/imgs/avatar-blank.png';
+        });
   }
+
+  // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fulfill(reader.result);
+      reader.readAsDataURL(blob);
+    })
+  }
+
+
   /*
     getCameraPicture(){
   
@@ -83,7 +106,7 @@ export class PerfilPage {
     this.clienteService.uploadPicture(this.picture)
       .subscribe(response => {
         this.picture = null;
-        this.loadData();
+        this.getImageIfExists();
         loader.dismiss();
       },
         error => {
